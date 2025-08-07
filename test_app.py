65d4612c29ef84bcfa463e70fa16973b7bef6e38
Shrika1420 <115211315+Shrika1420@users.noreply.github.com>
2025-08-06 19:56:54 -0700

    updated the test app as well

--- test_app.py
+++ test_app.py
@@ -1,15 +1,19 @@
 from app import app
 
-def test_home():
-    # Create a test client
-    tester = app.test_client()
-    response = tester.get('/')
+def test_dashboard():
+    with app.test_client() as client:
+        response = client.get('/dashboard')
+        
     
     #Check status code
     assert response.status_code == 200
     
-    # Chcek the content
+    # Checkk the content
+    assert response.data.decode('utf-8') == '<h1>Welcome to the Logistics Dashboard</h1>'
+    assert b'<img src="/logistics/delay-plot">' in response.data
+    assert b'<p>Total Shipments: 0</p>' in response.data
+    assert b'<p>Delayed Shipments: 0</p>' in response.data
+    assert b'<p>Average Delay: 0.0 minutes</p>' in response.data    
     
-    assert response.get_json() == {"message" : "Hello, CI/CD World!"} 
     
     
