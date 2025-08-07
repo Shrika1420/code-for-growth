8b0f672973b557ccf66c706ccd78052183985506
Shrika1420 <115211315+Shrika1420@users.noreply.github.com>
2025-08-06 23:30:17 -0700

    made changes in the test_app.py

--- test_app.py
+++ test_app.py
@@ -3,17 +3,17 @@ from app import app
 def test_dashboard():
     with app.test_client() as client:
         response = client.get('/dashboard')
+        html = response.data.decode('utf-8')
         
     
     #Check status code
     assert response.status_code == 200
     
     # Checkk the content
-    assert response.data.decode('utf-8') == '<h1>Welcome to the Logistics Dashboard</h1>'
-    assert b'<img src="/logistics/delay-plot">' in response.data
-    assert b'<p>Total Shipments: 0</p>' in response.data
-    assert b'<p>Delayed Shipments: 0</p>' in response.data
-    assert b'<p>Average Delay: 0.0 minutes</p>' in response.data    
+    assert "Welcome to the Logistics Dashboard" in html
+    assert "Total Shipments:" in html
+    assert "Delayed Shipments:" in html
+    assert "Average Delay:" in html  
     
     
     
